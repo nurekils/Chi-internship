ae2d36616efa5c64d754695a2af0f94516276cb1
Mac <mac@Macs-MacBook-Pro.local>
2026-01-26 03:59:15 +0100

    Moved styles to separate folder

--- 3 homework/books-app/src/main.tsx
+++ 3 homework/books-app/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import BooksApp from "./BooksApp";
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BooksApp />
